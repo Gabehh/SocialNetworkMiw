2c8079e67a1ed3d5060980dd21c11f3ff9b2487b
Gabriel Enrique Macho <gabe3195@gmail.com>
2020-06-15 03:12:17 +0200

    Chat version 1.0

---
+++ SocialNetworkMiw/Controllers/ChatController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using SocialNetworkMiw.Models;
+
+namespace SocialNetworkMiw.Controllers
+{
+    [Authorize]
+    public class ChatController : Controller
+    {
+        private readonly MongoClient mongoClient;
+        private readonly IMongoCollection<User> collectionUser;
+
+        public ChatController(IConfiguration configuration)
+        {
+            mongoClient = new MongoClient(configuration.GetConnectionString("SocialNetwork"));
+            collectionUser = mongoClient.GetDatabase("SocialNetworkMIW").GetCollection<User>("Users");
+        }
+
+        // GET: Chat
+        public ActionResult Index(string id)
+        {
+            return View(collectionUser.Find(new BsonDocument("$where", "this._id == '" + id + "'")).Single());
+        }
+    }
+}
--- SocialNetworkMiw/Startup.cs
+++ SocialNetworkMiw/Startup.cs
@@ -1,12 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
+using SignalRChat.Hubs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -31,6 +28,7 @@ namespace SocialNetworkMiw
             {
                 options.IdleTimeout = TimeSpan.FromSeconds(3600);
             });
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +60,7 @@ namespace SocialNetworkMiw
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapHub<ChatHub>("/chathub");
             });
         }
     }
